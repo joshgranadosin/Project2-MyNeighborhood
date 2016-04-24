c44acbae631718529a189e8ce7cf4af07d4e102c
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-04-23 19:09:31 -0700

    Figured out how to do api calls to google places

--- index.js
+++ index.js
@@ -18,12 +18,20 @@ app.use(bodyParser.json());
 var express = require("express");
 var request = require("request");
 
+// Constants
+var GOOGLEPLACESAPI = "https://maps.googleapis.com/maps/api/place/nearbysearch/";
+var GOOGLEPLACESOUTPUT = "json";
+var RADIUS = 1600;
+
+
 // Dummy Data
 var startAddress = { // used for testing
 	address: '16230 NE 99TH ST',
 	city: 'REDMOND',
 	state: 'WA',
-	zip: '98052'
+	zip: '98052',
+	lati: 47.6891625,	// pos for north (All of USA)
+	long: -122.1256417	// neg for west (All of USA)
 }
 
 var endCity = { // used for testing
@@ -31,25 +39,30 @@ var endCity = { // used for testing
 	state: 'WA'
 }
 
-// Default search filters
-var searchFilters = { // true for what you care is/isn't there. false if it don't matter
-	// family
-	park: true,
-	school: true,
-	library: false,
-	pet_store: false,
-	// convieniece
-	meal_delivery: false,
-	grocery_or_supermarket: false,
-	gas_station: false,
-	bus_station: false,
-	// life style
-	gym: false,
-	cafe: false,
-	night_club: false,
-	bar: true
-}
 
+// Default search filters
+var searchFilters = {
+	convenience: {
+		enabled: false,
+		types: ["bank", "grocery_or_supermarket", "post_office", "meal_delivery", "convenience_store"]
+	},
+	transportation: {
+		enabled: false,
+		types: ["transit_station", "bus_station", "gas_station"]
+	},
+	family: {
+		enabled: false,
+		types: ["school", "hospital", "police"]
+	},
+	lifestyle: {
+		enabled: true,
+		types: ["bar", "cafe", "church", "pet_store", "gym"]
+	},
+	recreation: {
+		enabled: false,
+		types: ["movie_theater", "park"]
+	}
+}
 
 // Throw away, read seattle xml
 app.get('/Seattle', function(req, res) {
@@ -59,5 +72,19 @@ app.get('/Seattle', function(req, res) {
 		res.send(json);
 	});
 });
+
+// Throw away, get google's data about 16230
+app.get('/Redmond', function(req, res) {
+	var type = "movie_theater";
+	var requestURL = GOOGLEPLACESAPI + GOOGLEPLACESOUTPUT
+		+ "?location=" + startAddress.lati + "," + startAddress.long + "&radius=" + RADIUS
+		+ "&type=" + type + "&key=" + process.env.GOOGLE_PLACES_API_KEY;
+
+	request(requestURL, function(err, response, body) {
+		res.send(body);
+	});
+});
+
+
 
 app.listen(process.env.PORT || 3000);
